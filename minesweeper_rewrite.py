9d3c62f91a52541ced8f622504512e4981af9fc4
Maple-YZ <zihanyee@gmail.com>
2024-03-28 12:03:38 +0800

    添加坐标系转换函数及窗体初始化

--- minesweeper_rewrite.py
+++ minesweeper_rewrite.py
@@ -1,13 +1,31 @@
 import pygame
 
 class Game():
-    
-    def __init__(self) -> None:
-        pass
-    
-    def run(self):
-        pass
-    
+
+    def __init__(self, window_size = (375, 375), ) -> None:
+        self._window_size = window_size
+        self.screen = pygame.display.set_mode(self._window_size)
+
+    def config(self, window_size=None):
+        if window_size:
+            self._window_size = window_size
+            self.screen
+
+    def run(self) -> None:
+        while True:
+            pass
+
+    def draw(self, slot, draw_type):
+        self.screen.blit(draw_type, self.slot_to_pos(slot))
+
+    @staticmethod
+    def slot_to_pos(slot: tuple[int]) -> tuple[int]:
+        return ((slot[0] + 3) * 25, (slot[1] + 3) * 25)
+
+    @staticmethod
+    def pos_to_slot(self, pos: tuple[int]) -> tuple[int]:
+        return (pos[0] // 25 - 3, pos[1] // 25 - 3)
+
 if __name__ == "__main__":
     game = Game()
     game.run()
